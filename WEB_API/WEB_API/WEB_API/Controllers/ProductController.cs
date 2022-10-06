2111fe3e8c81d5e3b731a6d83fbc5e685bafd52c
GBM-YADAV <giribabu00004@gmail.com>
2022-10-06 12:21:38 +0530

    commited

---
+++ WEB_API/WEB_API/WEB_API/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WEB_API.Controllers.Models;
+
+namespace WEB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        public List<Product> prodlist { get; set; }
+
+        public ProductController()
+        {
+            prodlist = new List<Product>()
+            {
+            new Product(){Id=1,ProductName="Dairy Milk",Price=50},
+            new Product(){Id=2,ProductName="Silk",Price=50},
+            new Product(){Id=3,ProductName="Kit Kat",Price=50},
+            new Product(){Id=1,ProductName="Snikers",Price=50},
+            new Product(){Id=1,ProductName="Munch",Price=50},
+
+            };
+        }
+        public ActionResult Get()
+        {
+            var data = prodlist;
+            return Ok(data);
+        }
+
+    }
+}
---
+++ WEB_API/WEB_API/WEB_API/Models/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WEB_API.Controllers.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
